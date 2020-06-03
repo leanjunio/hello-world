d53010ce0d881c57522d4aabf5e83932d1502dcf
Lean Junio <leanjunio@live.com>
2020-06-02 21:24:31 -0400

    update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello world!'));
-app.get('/abc', (req, res) => res.send('Hello world!'));
+app.get('/ab', (req, res) => res.send('Hello world!'));
 app.get('/api', (req, res) => res.send('hello world from api'));
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
