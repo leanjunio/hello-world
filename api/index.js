2f9692873d71552896f8894a12d61f2e275c58b1
Lean Junio <leanjunio@live.com>
2020-06-02 20:22:02 -0400

    first commit

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/ab', (req, res) => res.send('Hello world!'));
+app.get('/api', (req, res) => res.send('hello world from api'));
+
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
