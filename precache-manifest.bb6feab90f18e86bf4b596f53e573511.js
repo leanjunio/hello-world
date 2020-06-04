75d16f4d45386385fe8e7b92df11d0f87c2399e2
leanjunio <leanjunio@users.noreply.github.com>
2020-06-04 01:45:23 +0000

    deploy: e4496a22f3fa2459ad41fa0807788fa0b7295bc7

---
+++ precache-manifest.bb6feab90f18e86bf4b596f53e573511.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21660db11eef366c856f3abf40117c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e86100dcb1fe80255fb4",
+    "url": "/static/css/main.b4a81eb3.chunk.css"
+  },
+  {
+    "revision": "d4793c8cbff2908291f5",
+    "url": "/static/js/2.f0c9aa7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0c9aa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86100dcb1fe80255fb4",
+    "url": "/static/js/main.83f509cd.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
